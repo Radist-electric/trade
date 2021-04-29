544e5c00a6954cde29fd6ae1610775ac20888814
Radist <radist-electric@yandex.ru>
2021-04-29 21:54:46 +0500

    I displayed the data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 
 export const App = () => {
   const [values, setValues] = useState({
@@ -8,41 +8,48 @@ export const App = () => {
     sumOfDeviations: 0,
     standardDeviation: 0
   })
-
-  //ComponentDidMount
-  useEffect(() => {
-    console.log('ComponentDidMount')
-  }, [])// eslint-disable-line react-hooks/exhaustive-deps
-
+  const [show, setShow] = useState({
+    average: 0,
+    standardDeviation: 0
+  })
+  const [connect, setConnect] = useState(false)
+  const [timer, setTimer] = useState({
+    start: null,
+    diff: null
+  })
 
   const createConnection = () => {
-    console.log('createConnection')
     const socket = new WebSocket('wss://trade.trademux.net:8800/?password=1234')
 
     // Слушаем открытие соединения
     socket.onopen = function (e) {
-      console.log('[open] Соединение установлено')
+      console.log('Соединение установлено')
+      setConnect(true)
+      setTimer({
+        start: Date.now(),
+        diff: null
+      })
       try {
-        socket.send("ping")
+        socket.send('ping')
       } catch (err) {
-        console.log("Got invalid message from websocket on open", err, e.data);
+        console.log('Получено сообщение об ошибке при открытии соединения:', err, e.data)
+        setConnect(false)
       }
     }
 
     // Слушаем закрытие соединения
     socket.onclose = function (event) {
       if (event.wasClean) {
-        console.log(`[close] Соединение закрыто чисто, код=${event.code} причина=${event.reason}`)
+        console.log(`Соединение закрыто чисто, код=${event.code} причина=${event.reason}`)
       } else {
-        // например, сервер убил процесс или сеть недоступна
-        // обычно в этом случае event.code 1006
-        console.log('[close] Соединение прервано')
+        console.log('Соединение прервано')
       }
     }
 
     // Слушаем ошибки соединения
     socket.onerror = function (error) {
       console.log(`[error] ${error.message}`)
+      setConnect(false)
     }
 
     // Слушаем наличие сообщения
@@ -67,13 +74,31 @@ export const App = () => {
   }
 
   const getStatistics = () => {
-    console.log(values)
+    setShow({
+      average: values.average,
+      standardDeviation: values.standardDeviation
+    })
+    setTimer({...timer, diff: Date.now() - timer.start})
+  }
+
+  function getTime(ms) {
+    const milliseconds = parseInt((ms % 1000) / 100),
+      seconds = Math.floor((ms / 1000) % 60),
+      minutes = Math.floor((ms / (1000 * 60)) % 60),
+      hours = Math.floor((ms / (1000 * 60 * 60)) % 24),
+      days = Math.floor((ms / (1000 * 60 * 60 * 24)) % 365)
+    
+    return `${days}дн ${hours}ч ${minutes}мин ${seconds}.${milliseconds}с`
   }
 
   return (
-    <div className="App">
-      <button onClick={createConnection}>Старт</button>
-      <button onClick={getStatistics}>Статистика</button>
+    <div className='App'>
+      <h1>Данные по котировкам</h1>
+      <button className='button' onClick={createConnection} style={{ backgroundColor: connect ? '#4caf50' : '#1976d2' }}>Старт</button>
+      <button className='button' onClick={getStatistics}>Статистика</button>
+      <p>Среднее значение: <span className='value'>{show.average.toFixed(3)}</span></p>
+      <p>Стандартное отклонение: <span className='value'>{show.standardDeviation.toFixed(3)}</span></p>
+      <p>Время расчётов: <span className='value'>{getTime(timer.diff)}</span></p>
     </div>
   )
 }
